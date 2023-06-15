42ffad71f92c271e93631482847bce3bc4657bf1
Robert <rlhead94@gmail.com>
2023-06-14 20:37:25 -0400

    push

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, })
 
-// Middleware
-app.use(cors());
-app.use(express.json());
 
 const connection = mongoose.connection;
 connection.once('open', () =>{
     console.log("MongoDB database connection established succesfully");
 })
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // Routes
 
